0f3481c523d23d824a1ba600eaa23f3dfd1fe646
Stephen Munabo <33493565+stephenmunabo@users.noreply.github.com>
2020-10-09 01:05:15 -0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'zot_db' );
+define( 'DB_NAME', 'zot_base' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'zot_man' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'thisisthezot' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
